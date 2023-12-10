95c70f62bf1f08779d352463c8121552e57d4c63
TimofeyShved <timofeya123@inbox.ru>
2023-12-11 00:21:02 +0600

    new file xml

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,42 @@
 package com.company;
 
+import javax.xml.stream.*;
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws FileNotFoundException, XMLStreamException {
+        // Что бы прочитать XML файл, нужно создать сборщик ( ◡‿◡ *)
+        XMLOutputFactory factory = XMLOutputFactory.newFactory();
+        XMLStreamWriter streamWriter = factory.createXMLStreamWriter(new FileOutputStream("text.xml"));
+
+        streamWriter.writeStartDocument();
+        streamWriter.writeStartElement("bookstore");
+
+            streamWriter.writeStartElement("book");
+            streamWriter.writeAttribute("category", "COOKING");
+                    streamWriter.writeStartElement("title");
+                        streamWriter.writeCharacters("Everyday Italian");
+                    streamWriter.writeEndElement();
+            streamWriter.writeEndElement();
+
+            streamWriter.writeStartElement("book");
+            streamWriter.writeAttribute("category", "CHILDREN");
+                    streamWriter.writeStartElement("title");
+                        streamWriter.writeCharacters("Everyday Italian");
+                    streamWriter.writeEndElement();
+            streamWriter.writeEndElement();
+
+            streamWriter.writeStartElement("book");
+            streamWriter.writeAttribute("category", "WEB");
+                    streamWriter.writeStartElement("title");
+                        streamWriter.writeCharacters("Everyday Italian");
+                    streamWriter.writeEndElement();
+            streamWriter.writeEndElement();
+
+        streamWriter.writeEndElement();
+        streamWriter.writeEndDocument();
+
+
     }
 }
